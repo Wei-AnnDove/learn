3d3e13f8f6432355f7c1a41f0353c7ed61b6513c
滴答答滴滴答 <2263457981@qq.com>
2024-04-29 10:19:01 +0800

    -

--- c++programming/Projects/Projects.cpp
+++ c++programming/Projects/Projects.cpp
@@ -7,6 +7,10 @@ int main()
 {
     cout << "Beginning..." << endl;
     tt();
+
+    int a[5] = { 1,2,3,4,5 };
+    int* ptr = (int*)(a + 1);
+    cout << *(a + 1) << " " << *(ptr - 1) << endl;
 }
 
 
