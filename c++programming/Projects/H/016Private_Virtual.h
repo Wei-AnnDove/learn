18682d5dfd1aeab4976c7a1236c0467696b61078
滴答答滴滴答 <2263457981@qq.com>
2024-04-29 14:16:07 +0800

    创建 016Private_Virtual.h

---
+++ c++programming/Projects/H/016Private_Virtual.h
@@ -0,0 +1,15 @@
+#pragma once
+class A {
+	virtual void g();
+private:
+	virtual void f();
+};
+
+class B :public A {
+	void g();
+	virtual void h();
+};
+
+typedef void(*Fun)(void);
+
+void _016_Virtual_Function();
