b4c115e0349540c41da99673a17e64cbf4dd9ac3
滴答答滴滴答 <2263457981@qq.com>
2024-04-30 14:28:43 +0800

    更新 main.cpp

--- Programs/main.cpp
+++ Programs/main.cpp
@@ -3,6 +3,7 @@
 #include "H/008a_&a.h"
 #include "H/016Private_Virtual.h"
 #include "H/LinkedList.h"
+#include "H/022Stack_To_Queue.h"
 
 using namespace std;
 
@@ -10,10 +11,12 @@ int main()
 {
     cout << "Beginning..." << endl;
     Begin();
+    cout << endl;
 
     //_008_arrayAddress();
     //_016_Virtual_Function();
-    _020_reverse();
+    //_020_reverse();
+    _022_Stack_To_Queue();
 
     system("pause");
 
